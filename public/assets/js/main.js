fc98fc2f0335bb5ba9c6f2a1f4de14b4e0edd44d
krupa <krupa@karmadhi.com>
2025-04-17 18:40:54 +0530

    fix header

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -20695,19 +20695,25 @@ var Header = /*#__PURE__*/function () {
     key: "HeaderFixed",
     value: function HeaderFixed() {
       // header fixed js
-      var prevScrollPos = window.pageYOffset || document.documentElement.scrollTop;
-      $(window).scroll(function () {
-        var sticky = $(".header"),
-          scroll = $(window).scrollTop();
-        if (scroll >= 20) {
-          sticky.addClass("header-fixed");
-          sticky.removeClass("header-fixed-os");
-        } else {
-          sticky.removeClass("header-fixed");
-          sticky.addClass("header-fixed-os");
-        }
-        var currentScrollPos = window.pageYOffset || document.documentElement.scrollTop;
-        prevScrollPos = currentScrollPos;
+      $(document).ready(function () {
+        var prevScrollPos = $(window).scrollTop();
+        $(window).scroll(function () {
+          var sticky = $(".header"),
+            scroll = $(window).scrollTop();
+          if (scroll >= 50) {
+            sticky.addClass("header-fixed");
+            sticky.removeClass("header-fixed-os");
+          } else {
+            sticky.removeClass("header-fixed");
+            sticky.addClass("header-fixed-os");
+          }
+          if (prevScrollPos > scroll || scroll === 0) {
+            $(".header").removeClass("hidden");
+          } else {
+            $(".header").addClass("hidden");
+          }
+          prevScrollPos = scroll;
+        });
       });
     }
   }]);
--- src/js/parts/header.js
+++ src/js/parts/header.js
@@ -4,19 +4,29 @@ export class Header {
     }
     HeaderFixed() {
         // header fixed js
-        var prevScrollPos = window.pageYOffset || document.documentElement.scrollTop;
-        $(window).scroll(function () {
-            var sticky = $(".header"),
-                scroll = $(window).scrollTop();
-            if (scroll >= 20) {
-                sticky.addClass("header-fixed");
-                sticky.removeClass("header-fixed-os");
-            } else {
-                sticky.removeClass("header-fixed");
-                sticky.addClass("header-fixed-os");
-            }
-            var currentScrollPos = window.pageYOffset || document.documentElement.scrollTop;
-            prevScrollPos = currentScrollPos;
+        $(document).ready(function () {
+            var prevScrollPos = $(window).scrollTop();
+
+            $(window).scroll(function () {
+                var sticky = $(".header"),
+                    scroll = $(window).scrollTop();
+
+                if (scroll >= 50) {
+                    sticky.addClass("header-fixed");
+                    sticky.removeClass("header-fixed-os");
+                } else {
+                    sticky.removeClass("header-fixed");
+                    sticky.addClass("header-fixed-os");
+                }
+
+                if (prevScrollPos > scroll || scroll === 0) {
+                    $(".header").removeClass("hidden");
+                } else {
+                    $(".header").addClass("hidden");
+                }
+
+                prevScrollPos = scroll;
+            });
         });
     }
 }
